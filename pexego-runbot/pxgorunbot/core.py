2c830760560a683caac69a688fe2433ce987732e
Omar (Pexego) <omar@pexego.es>
2013-05-20 18:07:52 +0200

    [IMP] pexego_runbot: 5.0 compatible.

--- pexego-runbot/pxgorunbot/core.py
+++ pexego-runbot/pxgorunbot/core.py
@@ -295,7 +295,7 @@ class Job(object):
         openerp.server.timeout = 450
         """%(self.client_web_port+port,self.server_net_port+port,self.server_net_port+port)
         config=config.replace("\n        ","\n")
-        cfgs = [os.path.join(self.client_web_path,"doc","openerp-web.cfg"), os.path.join(self.client_web_path,"openerp-web.cfg")]
+        cfgs = [os.path.join(self.client_web_path,"doc","openerp-web.cfg"), os.path.join(self.client_web_path,"openerp-web.cfg"), os.path.join(self.client_web_path,"config","openerp-web.cfg")]
         for i in cfgs:
             f=open(i,"w")
             f.write(config)
