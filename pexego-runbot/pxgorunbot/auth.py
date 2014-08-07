577ba9a88519ac5af466b77a70613df2a29c268a
jesusVMayor <jesus@pexego.es>
2014-08-07 13:55:14 +0200

    Marcados módulos no migrados como no instalables.

--- pexego-runbot/pxgorunbot/auth.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from app import app
-from database import db
-from models import User
-from flask_peewee.auth import Auth
-
-auth = Auth(app, db, user_model=User, prefix='')
--- pexego-runbot/pxgorunbot/database.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from app import app
-from flask_peewee.db import Database
-
-database = Database(app)
-db = database.database
